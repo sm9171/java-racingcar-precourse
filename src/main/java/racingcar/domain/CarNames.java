a920b1a39b584e6e5a69bbd653dad5ea988bfa85
sm9171 <sm9171@nate.com>
2022-10-07 13:27:54 +0900

    feature(CarNames): CarNames Model 생성

---
+++ src/main/java/racingcar/domain/CarNames.java
@@ -0,0 +1,39 @@
+package racingcar.domain;
+
+import racingcar.common.Input;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class CarNames {
+
+	private final Set<CarName> names;
+
+	private final static String SPLIT_REGEX = ",";
+
+	private CarNames() {
+		this.names = initNames();
+	}
+
+	public static CarNames fromConsole() {
+		return new CarNames();
+	}
+
+	public Set<CarName> names() {
+		return this.names;
+	}
+
+	private Set<CarName> initNames() {
+		// TODO refactoring at view model
+		System.out.println("자동차 이름을 입력해주세요.");
+		Set<CarName> names = new LinkedHashSet<>();
+
+		for (String name
+				: Input.fromConsole().splitToString(SPLIT_REGEX)) {
+			names.add(CarName.of(name));
+		}
+
+		return names;
+	}
+
+}
