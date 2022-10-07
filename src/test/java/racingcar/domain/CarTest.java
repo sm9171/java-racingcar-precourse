21ab741c93166bc4ab5477db4797715ab0836f29
sm9171 <sm9171@nate.com>
2022-10-07 13:12:31 +0900

    feature(Car): Car Model 생성

---
+++ src/test/java/racingcar/domain/CarTest.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+	@Test
+	void Car인스턴스_생성() {
+		assertDoesNotThrow(() -> Car.of(CarName.of("car")));
+	}
+
+	@Test
+	void Car_객체_동일성_체크() {
+		Car left = Car.of(CarName.of("car"));
+		Car right = Car.of(CarName.of("car"));
+
+		assertEquals(left, right);
+	}
+}
